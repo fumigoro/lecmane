749f0d6868330d1d1da6c9acb0f5ace307cd2d42
fumigoro <51395778+fumigoro@users.noreply.github.com>
2023-03-21 04:14:56 +0900

    設定のボタンにハンドラ追加

--- src/pages/Setting.page.tsx
+++ src/pages/Setting.page.tsx
@@ -3,20 +3,31 @@ import PageWrapper from '../components/general/BackgroundWrapper';
 import { Header } from '../components/general/Header';
 import { InAppLink } from '../components/general/InAppLink';
 import MobileNavigation from '../components/general/Navigation';
+import { StorageIO, Key } from '../lib/storage';
 
 const SettingPage = () => {
   return (
     <PageWrapper>
       <Header pageTitle="設定" />
       <Container sx={{ my: 2 }}>
-        <Box py={2}>
+        <Box py={2} onClick={() => {
+          localStorage.clear();
+          window.alert('お気に入り登録を削除しました');
+        }}>
           <Typography gutterBottom>お気に入りをリセット</Typography>
           <Typography variant="body2" color="gray">
             講義のお気に入り登録を削除します
           </Typography>
         </Box>
         <Divider />
-        <Box py={2}>
+        <Box py={2} onClick={() => {
+          window.alert('キャッシュを削除しました');
+          const data = StorageIO.get(Key.FAVORITE_LIST);
+          localStorage.clear();
+          if (data) {
+            StorageIO.set(Key.FAVORITE_LIST, data);
+          }
+        }}>
           <Typography gutterBottom>キャッシュを削除</Typography>
           <Typography variant="body2" color="gray">
             一時保存している講義データを削除します
