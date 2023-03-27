3c1694be30f11c5341af8a18ec1384df7f472c71
fumigoro <51395778+fumigoro@users.noreply.github.com>
2023-03-27 12:14:29 +0900

    ビルドツールをViteへ移行

--- app/src/env.d.ts
+++ app/src/env.d.ts
@@ -2,9 +2,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_CUSTOM_ENV: string
+  readonly VITE_CUSTOM_ENV: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
