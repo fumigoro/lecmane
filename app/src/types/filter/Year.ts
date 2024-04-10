5f1e7473d9fedaad25a8f2ecd3d925b7814483e9
fumigoro <51395778+fumigoro@users.noreply.github.com>
2024-04-11 01:51:05 +0900

    2024シラバス対応

--- app/src/types/filter/Year.ts
+++ app/src/types/filter/Year.ts
@@ -1,9 +1,13 @@
-export type Year = 2023 | 2022;
+export type Year = 2024 | 2023 | 2022;
 
 export const years: {
   value: Year;
   label: string;
 }[] = [
+  {
+    value: 2024,
+    label: '2024(令和6)年度'
+  },
   {
     value: 2023,
     label: '2023(令和5)年度'
